cc1a0427647915be8ca3fd738f8e1183ea26e098
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2024-04-01 17:06:38 +0100

    added nix flake file

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    description = "Optimizing a SECD machine";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
+
+    outputs = { self, ... } @ inputs: 
+    let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        ROOT = builtins.getEnv "PWD";
+        name = "SECD_Optimization";
+        system = "x86_64-linux";
+    in {
+        devShells."${system}".default = pkgs.mkShell {
+            inherit name ROOT;
+
+            buildInputs = with pkgs; [
+                haskell.compiler.ghc98
+                haskellPackages.cabal-install 
+            ];
+
+            shellHook = ''
+                export NIX_SHELL_NAME="${name}" 
+                echo -ne "\033]0;${name}\007"
+
+                reload
+            '';
+
+        };
+    };
+}
